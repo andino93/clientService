eabbbe3cf5f59be2a9c91b13b1822fb8b669c13f
Andrew Nguyen <andy.andino93@gmail.com>
2017-12-12 16:04:20 -0800

    add db connection and tests for basic funcs

---
+++ database/index.js
@@ -0,0 +1,28 @@
+import { config } from 'dotenv';
+import elasticsearch from 'elasticsearch';
+import Promise from 'bluebird';
+
+config();
+const search = new elasticsearch.Client({
+  hosts: process.env.ELASTIC,
+  defer: () => Promise.defer(),
+});
+
+search.ping({ requestTimeout: 30000 })
+  .then(() => console.log('elasticity!'))
+  .catch(err => console.error(err));
+
+const addNewEntry = (location, type, id, body) => (
+  search.index({
+    index: location,
+    type,
+    id,
+    body,
+  })
+);
+
+const deleteEntry = (index, type, id) => (
+  search.delete({ index, type, id })
+);
+
+export { addNewEntry, deleteEntry };
--- spec/database.spec.js
+++ spec/database.spec.js
@@ -1,9 +1,17 @@
-import assert from 'assert';
+import { expect, assert } from 'chai';
+import { config } from 'dotenv';
+import Promise from 'bluebird';
+import { addNewEntry, deleteEntry } from '../database/index';
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+describe('Database should do things', () => {
+  describe('should add new entries to database', () => {
+    it('addNewEntry should exist', () => {
+      expect(addNewEntry).to.exist;
+    });
+  });
+  describe('should delete entries from database', () => {
+    it('deleteEntry should exist', () => {
+      expect(deleteEntry).to.exist;
     });
   });
 });
