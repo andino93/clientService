6455ba0f4cbaf2cf3cf9be9b6d4f9f0aded85d52
Andrew Nguyen <andy.andino93@gmail.com>
2017-12-19 14:33:51 -0800

    Feat/search (#2)
    * init package.json and starter folders
    
    * init index.js for server and db
    
    * airbnblinter
    
    * add eslintrc
    
    * bablerc
    
    * config babel for live reload
    
    * add db connection and tests for basic funcs
    
    * add search, add, and delete funcs
    
    * some working queries with route
    
    * add working routes for experience desision.
    
    * sample data
    
    * modified funcs for insertion
    
    * add npm run script
    
    * change write params
    
    * add uuid
    
    * fix experience add to response
    
    * refactor routes and add more tests for queue
    
    * fix data seed
    
    * add catch fallback
    
    * add database tests
    
    * fix sample generation
    
    * add test for search

---
+++ database/index.js
@@ -0,0 +1,49 @@
+import { config } from 'dotenv';
+import elasticsearch from 'elasticsearch';
+import Promise from 'bluebird';
+import uuid from 'uuid/v4';
+
+config();
+
+const client = new elasticsearch.Client({
+  hosts: process.env.ELASTIC,
+  index: 'rentals',
+  defer: () => Promise.defer(),
+});
+
+client.ping({ requestTimeout: 30000 })
+  .then(res => console.log('elasticity!', res))
+  .catch(err => console.error(err));
+
+const addNewEntry = (location, index, body, type) => (
+  client.index({
+    index,
+    type,
+    id: uuid(),
+    body,
+  })
+);
+
+const bulkInsert = params => (
+  client.bulk(params)
+);
+
+const deleteEntry = (index, type, id) => (
+  client.delete({ index, type, id })
+);
+
+const searchIt = (index, type, query, key = 'city') => (
+  client.search({
+    index,
+    type,
+    body: {
+      query: {
+        match: {
+          [key]: query,
+        },
+      },
+    },
+  })
+);
+
+export { addNewEntry, deleteEntry, searchIt, bulkInsert };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import { addNewEntry, searchIt } from '../database/index';
+import addExperience from './addExperience';
 
 config();
 const app = express();
@@ -9,7 +12,23 @@ const port = process.env.PORT;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.sendStatus(200);
+app.get('/rentals?:location', (req, res) => {
+  // TODO: remove res.send result/err after testing is done
+  let { location, showExperience } = req.query; // eslint-disable-line
+  showExperience = showExperience || Math.random() > 0.5;
+  searchIt('homes', 'home', location)
+    .then(query => addExperience(query, showExperience, location))
+    .then(result => res.send(result))
+    .catch(err => res.send(err));
+});
+
+app.post('/rentals', (req, res) => {
+  // TODO: remove res.send result/err after testing is done
+  const { location, index, body } = req.body;
+  addNewEntry(location, index, body)
+    .then(result => res.send(result))
+    .catch(err => res.send(err));
 });
