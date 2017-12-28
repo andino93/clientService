96ae49dbf4ce1ec617dc9512af1f1170e8f6596f
Andrew Nguyen <andy.andino93@gmail.com>
2017-12-27 21:28:32 -0800

    update helper spec with tests

--- spec/helpers.spec.js
+++ spec/helpers.spec.js
@@ -2,7 +2,7 @@ import { expect, assert } from 'chai';
 import { config } from 'dotenv';
 import "babel-polyfill"; // for async/await compiling
 import addExperience from '../helpers/addExperience';
-import generateData from '../helpers/generateData';
+import { populateHomes, populateExperiences, makeSampleData } from '../helpers/generateData';
 import getUrl from '../helpers/details';
 
 config();
@@ -26,9 +26,43 @@ describe('Helper Functions', () => {
         expect(addExp.experiences.hits[0]._source.city).to.equal('Chicago');
     });
   });
-  xdescribe('generateData should make fake data', () => {
+  describe('generateData should make fake data', () => {
     // TODO: determine tests for sample data
-
+    describe('makeSampleData', () => {
+      const sample = makeSampleData(1, 'homes', 'home', 'homeId', false);
+      it('sample data should be an array of objects', () => {
+        expect(sample).to.be.an('array');
+        expect(sample[0]).to.be.an('object');
+      });
+      it('sample data should resemble real data', () => {
+        expect(sample[0]).to.have.property('title');
+        expect(sample[0]).to.have.property('description');
+        expect(sample[0]).to.have.property('datesAvailable');
+        expect(sample[0]).to.have.property('city');
+        expect(sample[0]).to.have.property('suburb');
+        expect(sample[0]).to.have.property('price');
+        expect(sample[0]).to.have.property('homeId');
+        expect(sample[0]).to.have.property('guestCount');
+      });
+    });
+    describe('populateHomes', () => {
+      it('should insert homes into database', async () => {
+        const response = await populateHomes(10);
+        expect(response).to.be.an('object');
+        expect(response.items[0].index._index).to.equal('homes');
+        expect(response.items[0].index._type).to.equal('home');
+        expect(response.items[0].index.result).to.equal('created');
+      });
+    });
+    describe('populateExperiences', () => {
+      it('should insert experiences into database', async () => {
+        const response = await populateExperiences(10);
+        expect(response).to.be.an('object');
+        expect(response.items[0].index._index).to.equal('experiences');
+        expect(response.items[0].index._type).to.equal('experience');
+        expect(response.items[0].index.result).to.equal('created');
+      });
+    });
   });
   describe('getUrl should return endpoint based on input', () => {
     it('should return experiences url', () => {
